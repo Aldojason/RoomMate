92ef50b49a94f4972ec4840cd5c0c1892ea8227b
Aldojason <aldojason07@gmail.com>
2026-09-23 10:15:11 +0530

    Connect Flutter app to production backend

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -4,7 +4,7 @@ import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
 class ApiService {
-  static const String baseUrl = 'http://localhost:5000';
+  static const String baseUrl = 'https://roommate-bw1x.onrender.com';
 
   // ============================================================
   // AUTH HEADERS
